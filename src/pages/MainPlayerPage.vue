234d66652d6db71fa8900c791258754272770017
jonathan minguez de las rosa <jonathan.minguez.delarosa@alumnojoyfe@iepgroup.es>
2025-02-12 19:48:44 +0100

    Ventana de ultimas reproducciones

--- src/pages/MainPlayerPage.vue
+++ src/pages/MainPlayerPage.vue
@@ -1,13 +1,7 @@
 <template>
-<<<<<<< HEAD
     <div class="main-player-container">
         <div v-show="searchDataStore.loadSearch == true">
             <div v-for="(soundData, key) in soundsData" class="song-element">
-=======
-    <div class="main-player-container" @scroll="handleScroll">
-        <div v-show="searchDataStore.loadSearch">
-            <div v-for="(soundData, key) in soundsData" :key="key" class="song-element">
->>>>>>> 8f91dc52b4a588b14295ab2b8bdca47b262dd479
                 <div class="song-img">
                     <img :src="soundData.images ? soundData.images.spectral_m : ''">
                 </div>
@@ -26,11 +20,8 @@
                     <i class="bi bi-play-circle btn-song-play" @click="setCurrentSong(key)"></i>
                 </div>
             </div>
-<<<<<<< HEAD
 
             <div ref="loadTrigger" v-if="nextPage">Cargando más sonidos...</div>
-=======
->>>>>>> 8f91dc52b4a588b14295ab2b8bdca47b262dd479
         </div>
         <div class="loading-gif-container" v-show="!searchDataStore.loadSearch">
             <loadingComponent></loadingComponent>
@@ -39,11 +30,7 @@
 </template>
 
 <script setup>
-<<<<<<< HEAD
 import { onMounted, ref, watch, nextTick } from 'vue';
-=======
-import { onMounted, ref, watch } from 'vue';
->>>>>>> 8f91dc52b4a588b14295ab2b8bdca47b262dd479
 import { useSoundDataStore } from '../stores/soundData';
 import { useSearchStore } from '@/stores/search';
 import loadingComponent from '@/components/loadingComponent.vue';
@@ -52,23 +39,15 @@ import apiService from '../services/apiService';
 const searchDataStore = useSearchStore();
 const soundDataStore = useSoundDataStore();
 
-<<<<<<< HEAD
 let soundsData = ref({});
 let timeOutId = 0;
 let nextPage = ref(null);
 const loadTrigger = ref(null);
-=======
-let soundsData = ref([]);
-let nextUrl = ref(null);
-let cargaNextUrl = ref(false);
-let timeOutId = 0;
->>>>>>> 8f91dc52b4a588b14295ab2b8bdca47b262dd479
 
 watch(
     () => searchDataStore.searchText,
     () => {
         searchDataStore.loadSearch = false;
-<<<<<<< HEAD
         if(timeOutId != 0){
             clearTimeout(timeOutId)
             timeOutId = 0;
@@ -120,68 +99,3 @@ const setCurrentSong = (key) => {
     soundDataStore.soundUrl = soundsData.value[key].previews["preview-hq-mp3"];
 };
 </script>
-=======
-        if (timeOutId) {
-            clearTimeout(timeOutId);
-        }
-        timeOutId = setTimeout(getSoundsData, 1000);
-    }
-);
-
-onMounted(() => {
-    getSoundsData();
-});
-
-const getSoundsData = async () => {
-    let response = await apiService.getSounds(searchDataStore.searchText);
-    soundsData.value = response.results || [];
-    nextUrl.value = response.next || null;
-    
-    await Promise.all(
-        soundsData.value.map(async (element, key) => {
-            let data = await apiService.getsoundData(element.id);
-            soundsData.value[key] = { ...data, ...soundsData.value[key] };
-        })
-    );
-    
-    searchDataStore.loadSearch = true;
-};
-
-const getSoundsDataNext = async () => {
-    if (cargaNextUrl.value || !nextUrl.value) return;
-    
-    cargaNextUrl.value = true;
-    let response = await apiService.getSounds(nextUrl.value);
-    if (response && response.results) {
-        let newSounds = response.results;
-        nextUrl.value = response.next || null;
-        
-        await Promise.all(
-            newSounds.map(async (element) => {
-                let data = await apiService.getsoundData(element.id);
-                Object.assign(element, data);
-            })
-        );
-        
-        soundsData.value = [...soundsData.value, ...newSounds];
-    }
-    cargaNextUrl.value = false;
-};
-
-const handleScroll = (event) => {
-    const element = event.target;
-    if (element.scrollHeight - element.scrollTop === element.clientHeight) {
-        getSoundsDataNext();
-    }
-};
-
-const changeCurrentSong = (key) => {
-    soundDataStore.soundName = soundsData.value[key].name;
-    soundDataStore.soundImg = soundsData.value[key].images?.spectral_m || '';
-    soundDataStore.soundUrl = soundsData.value[key].previews?.["preview-hq-mp3"] || '';
-};
-</script>
-
-<style scoped>
-</style>
->>>>>>> 8f91dc52b4a588b14295ab2b8bdca47b262dd479
