8bab327449e32fc6a43316765ef0728951c22761
BrianPA15 <brian.perugachi.angulo@alumnojoyfe.iepgroup.es>
2025-02-12 20:25:34 +0100

    Brian: Mi ultima reproducciones

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,12 +1,8 @@
 export default {
 
     BASE_URL: "https://freesound.org/apiv2",
-<<<<<<< HEAD
     // TOKEN: "94QI2cQexWIEFtOEIBCoQOUja0rKDf0N3JXFuaGp",
     TOKEN: "w5uHxwaLh9vfaBwtSJj7MVqJHxVB4hYBsJVtoiqO",
-=======
-    TOKEN: "N4XAd06Sn5p3sOJmhFUtB9JOACb4BxctCWUn839Z",
->>>>>>> d9cc60fbd703585dffead56e245a55e65e1909ca
 
     async get(url) {
         url = url + "&token=" + this.TOKEN;
